718a2598d4b618f6f9723446b40a21d6c0818c5d
ssebak <stein.sebak@gmail.com>
2020-05-12 19:34:34 +0200

    Moved functions from the exacutable file over to the localFunctions.py file

--- localFunctions.py
+++ localFunctions.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generateRandomness(l):
+ef generateRandomness(l):
     rndString = ''.join(random.choice(string.ascii_lowercase + ' ') for x in range(l))
     return rndString
 
@@ -32,3 +32,61 @@ def humanreadableFilesize(n):
         return str(round(n/1000000,2)) + "GB"
     else:
         return str(round(n/1000000000,2)) + "TB"
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def createFiles(dirName,filesize,totalFiles):
+    global i
+    fileName = generateFilename(filenameLength,filenameExtension)
+    #filePath = fileName
+    filePath = os.path.join(dirName, fileName)
+    f = open (filePath,"w")
+    rndString = generateRandomness(filesize)
+    f.write (rndString)
+    f.close()
+
+    progress(i, totalFiles, status='Creating files')
+    i += 1
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def createFolder(createinDir,subdirCreated,totalFiles):
+    global subdirLevel
+    
+    for x in range(numDirectories):
+        dirName = os.path.join(createinDir,generateDirname(dirnameLength))
+        if not os.path.exists(dirName):
+            #print(dirName)
+            os.mkdir(dirName)
+            
+            
+            #os.chdir(dirName)
+            for y in range(numFilesPerDirectory):
+                createFiles(dirName,fileSize,totalFiles)
+            
+        if subdirLevel > 0:
+            if subdirCreated < subdirLevel:
+                createFolder(dirName,subdirCreated + 1,totalFiles)
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def calcHowmanyfolders(numFolders, subdirLevel, numFiles, kb):
+    foldersOnthislevel = numFolders         #level1
+    totalFolders = foldersOnthislevel
+    for x in range(1,subdirLevel+1):        #need to add 1 as range does not include last number
+        foldersOnthislevel = foldersOnthislevel * numFolders
+        totalFolders += foldersOnthislevel
+
+    totalFiles = totalFolders * numFiles
+    totalkb = totalFiles * kb / 1000
+    print()
+    run = input("A total of " + humanreadableNumber(totalFolders) + " folders and " + humanreadableNumber(totalFiles) + " files that will take appr " + humanreadableFilesize(totalkb) + " of diskspace will be created. Ok to continue (Y/N)?")
+
+    if run != 'Y' and run != 'y':
+        exit()
+    print()
+    print()
+    return totalFiles   #we need this to show the progress bar
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+def progress(count, total, status=''):
+    percents = round(100.0 * count / float(total), 1)
+
+    sys.stdout.write('%s%s ...%s\r' % (percents, '%', status))
--- randomFiles.py
+++ randomFiles.py
@@ -8,71 +8,14 @@ from localFunctions import generateFilename
 from localFunctions import generateDirname
 from localFunctions import humanreadableNumber
 from localFunctions import humanreadableFilesize
+from localFunctions import createFiles
+from localFunctions import createFolder
+from localFunctions import calcHowmanyfolders
+from localFunctions import progress
 
 i = 0
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-def createFiles(dirName,filesize,totalFiles):
-    global i
-    fileName = generateFilename(filenameLength,filenameExtension)
-    #filePath = fileName
-    filePath = os.path.join(dirName, fileName)
-    f = open (filePath,"w")
-    rndString = generateRandomness(filesize)
-    f.write (rndString)
-    f.close()
-
-    progress(i, totalFiles, status='Creating files')
-    i += 1
-
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-def createFolder(createinDir,subdirCreated,totalFiles):
-    global subdirLevel
-    
-    for x in range(numDirectories):
-        dirName = os.path.join(createinDir,generateDirname(dirnameLength))
-        if not os.path.exists(dirName):
-            #print(dirName)
-            os.mkdir(dirName)
-            
-            
-            #os.chdir(dirName)
-            for y in range(numFilesPerDirectory):
-                createFiles(dirName,fileSize,totalFiles)
-            
-        if subdirLevel > 0:
-            if subdirCreated < subdirLevel:
-                createFolder(dirName,subdirCreated + 1,totalFiles)
-
-
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-def calcHowmanyfolders(numFolders, subdirLevel, numFiles, kb):
-    foldersOnthislevel = numFolders         #level1
-    totalFolders = foldersOnthislevel
-    for x in range(1,subdirLevel+1):        #need to add 1 as range does not include last number
-        foldersOnthislevel = foldersOnthislevel * numFolders
-        totalFolders += foldersOnthislevel
-
-    totalFiles = totalFolders * numFiles
-    totalkb = totalFiles * kb / 1000
-    print()
-    run = input("A total of " + humanreadableNumber(totalFolders) + " folders and " + humanreadableNumber(totalFiles) + " files that will take appr " + humanreadableFilesize(totalkb) + " of diskspace will be created. Ok to continue (Y/N)?")
-
-    if run != 'Y' and run != 'y':
-        exit()
-    print()
-    print()
-    return totalFiles   #we need this to show the progress bar
-
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-def progress(count, total, status=''):
-    percents = round(100.0 * count / float(total), 1)
-
-    sys.stdout.write('%s%s ...%s\r' % (percents, '%', status))
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-os.system('cls' if os.name == 'nt' else 'clear')
+system('cls' if os.name == 'nt' else 'clear')
 
 subdirLevel_default = 0
 numDirectories_default = 1
